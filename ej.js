d2ea114b92a9e791b7e89d800c2ad3daf02fc752
GerardoCL31 <gerardotrajano@gmail.com>
2025-11-17 19:49:19 +0100

    end

---
+++ ej.js
@@ -0,0 +1,34 @@
+
+const instrucciones = "LMLMLMLMM"
+
+function moverRover(x,y,dir,instrucciones){
+for (let i = 0; i < instrucciones.length; i++) {
+    const inst = instrucciones[i]
+
+    if(inst==="L"){
+        if(dir==="N")dir = "W"
+        else if(dir ==="W") dir ="S"
+        else if (dir==="S") dir ="E"
+        else if (dir === "E") dir ="N"
+    }
+
+    if(inst==="R"){
+        if (dir==="N")dir="E"
+        else if(dir==="E")dir="S"
+        else if(dir==="S")dir="W"
+        else if (dir==="W")dir="N"
+    }
+
+    if(inst==="M"){
+        if(dir==="N")y++
+        if(dir==="S")y--
+        if(dir==="W")x--
+        if(dir==="E")x++
+    }
+
+
+}
+console.log("La direccion final de rober es: "+x,y,dir)
+}
+moverRover(1,2,"N","LMLMLMLMM")
+moverRover(3,3,"E","MMRMMRMRRM")
